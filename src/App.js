14ff14639f6f3683a2378229c7600fb9abdc5b43
Hermann Szabó <zbo@Hermanns-MacBook-Pro.local>
2020-03-01 22:19:35 +0400

    adding styles.

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { robots } from './robots';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: '',
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+    }
+
+    render() {
+        const filterdRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        });
+        return (
+            <div className = 'tc'>
+                <h1 className = 'f1'>RoboFriends</h1>
+                <SearchBox searchChange = {this.onSearchChange} />
+                <CardList robots = {filterdRobots}/>
+            </div>
+            
+        );
+    }   
+}
+
+export default App;
