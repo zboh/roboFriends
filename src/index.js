642bbebd7e9661aff1fc5a411823ba7d4f9b64e3
Hermann Szabó <zbo@Hermanns-MacBook-Pro.local>
2020-03-01 17:03:19 +0400

    minor update.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card.js';
+import cardList from './cardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots';
 
-ReactDOM.render(<div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-        <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-        <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </div>, 
+ReactDOM.render(<cardList robots={robots}/>, 
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
