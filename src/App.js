54e0442aad646c3c3b7d4e75a885cad8cde64171
Hermann Szabó <zbo@Hermanns-MacBook-Pro.local>
2020-03-01 23:11:20 +0400

    add scroll-fixed searcbar and API.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import { robots } from './robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll'
 import './App.css';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: '',
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
     }
 
     onSearchChange = (event) => {
@@ -21,14 +27,19 @@ class App extends Component {
         const filterdRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         });
-        return (
-            <div className = 'tc'>
-                <h1 className = 'f1'>RoboFriends</h1>
-                <SearchBox searchChange = {this.onSearchChange} />
-                <CardList robots = {filterdRobots}/>
-            </div>
-            
-        );
+        if (this.state.robots.length === 0) {
+            return <h1 className = 'tc'>Loading</h1>
+        } else {
+            return (
+                <div className = 'tc'>
+                    <h1 className = 'f1'>RoboFriends</h1>
+                    <SearchBox searchChange = {this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots = {filterdRobots}/>
+                    </Scroll>
+                </div>   
+            );
+        }
     }   
 }
 
--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
     return (
         <div className='bg-light-purple dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={`https://robohash.org/${id}?size=200x200`} alt='Robot friend' />
+            <img src={`https://robohash.org/${id}?size=220x200`} alt='Robot friend' />
             <div className='tc'>
                 <h3>
                     {name}
